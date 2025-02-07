9ffaa38002f1714fbd9b3ca891ba90eedf773c95
1234567890925 <mandapmalvirutuja@gmail.com>
2025-02-07 14:41:21 -0800

    complexities for array partition

--- 561-Array-Partition.py
+++ 561-Array-Partition.py
@@ -8,3 +8,9 @@ class Solution:
         for j, k in pairs:
             min_sum += j
         return min_sum
+    
+# Time complexity is O(nlogn) as sorting (merge sort) takes O(nlogn)
+# [  O(logn) for splitting the elements AND
+#    O(n) for merging the elements together]
+
+# Space complexity is O(n) as we are just using the same array
