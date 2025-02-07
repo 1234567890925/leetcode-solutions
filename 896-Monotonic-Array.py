8109b944bfe88e282a8978e252d3db2cf2ab99d6
RUTUJA MANDAPMALVI <76784381+1234567890925@users.noreply.github.com>
2025-02-07 15:02:52 -0800

    [896] [Time Beats: 67.42%] [Memory Beats: 73.24%] - LeetPush

---
+++ 896-Monotonic-Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if all(nums[i] <= nums[i+1] for i in range(len(nums)-1)):
+            return True
+        if all(nums[i] >= nums[i+1] for i in range(len(nums)-1)):
+            return True
+        else:
+            return False
