c549c9d39a46ce19d62c361e3324d8c79037ba4a
RUTUJA MANDAPMALVI <76784381+1234567890925@users.noreply.github.com>
2025-02-11 00:22:27 -0800

    [13] [Time Beats: 65.31%] [Memory Beats: 60.55%] - LeetPush

---
+++ 13-Roman-to-Integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        letter_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+            'C': 100, 'D': 500, 'M': 1000
+        }
+
+        total = 0
+        for i in range(len(s)-1):
+            if letter_map[s[i]] < letter_map[s[i+1]]:
+                total -= letter_map[s[i]]
+            else:
+                total += letter_map[s[i]]
+        total += letter_map[s[-1]]
+        return total
