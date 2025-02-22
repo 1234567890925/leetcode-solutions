7eadd6560d51a4e2270258a2244a81cf50807a32
RUTUJA MANDAPMALVI <76784381+1234567890925@users.noreply.github.com>
2025-02-22 13:46:56 -0800

    [121] [Time Beats: 46.91%] [Memory Beats: 33.62%] - LeetPush

--- 121-Best-Time-to-Buy-and-Sell-Stock.py
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,9 +1,13 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         max_profit = 0
-        min_price = float('inf')
-        for price in prices:
-            min_price = min(min_price, price)
-            profit = price - min_price
-            max_profit = max(max_profit, profit)
+        l, r = 0, 1
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+            else:
+                l = r
+            r += 1
         return max_profit
+            
