8a7c2682739d91bfa3e0ba38db9ca2bc11a30040
RUTUJA MANDAPMALVI <76784381+1234567890925@users.noreply.github.com>
2025-02-21 19:12:35 -0800

    [347] [Time Beats: 91.21%] [Memory Beats: 80.65%] - LeetPush

---
+++ 347-Top-K-Frequent-Elements.py
@@ -0,0 +1,13 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = [] #heap
+        counter = {} #hashmap
+        for n in nums:
+            counter[n] = 1 + counter.get(n, 0) #default values of counter is 0
+        for key, val in counter.items():
+            heapq.heappush(heap, (-val, key))
+
+        res=[]
+        while len(res) < k:
+            res.append(heapq.heappop(heap)[1])
+        return res
