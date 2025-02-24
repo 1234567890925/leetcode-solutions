36ec7f444450637aa2aaf91fb7de4f08fea4fd5a
RUTUJA MANDAPMALVI <76784381+1234567890925@users.noreply.github.com>
2025-02-24 12:29:11 -0800

    [1] [Time Beats: 51.56%] [Memory Beats: 34.24%] - LeetPush

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -1,6 +1,18 @@
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}
+        #hash map
+        for i, val in enumerate(nums):
+            indices[val] = i
+        for i, val in enumerate(nums):
+            diff = target - val
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+
+'''
         for i in range(0, len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target and [i] != [j]:
                     return ([i, j])
+'''
+
