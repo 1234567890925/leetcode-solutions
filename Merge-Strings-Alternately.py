3474cf1afd823467bc824c74d81464f876d8553d
RUTUJA MANDAPMALVI <76784381+1234567890925@users.noreply.github.com>
2025-07-16 00:47:09 -0700

    [1768] [Time Beats: 8.06%] [Memory Beats: 83.70%] - LeetPush

--- Merge-Strings-Alternately.py
+++ Merge-Strings-Alternately.py
@@ -1,15 +1,23 @@
-# Time complexity = O(n)
-# Space complexity = O(n)
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        i = j = 0
-        res = []
-        while i < len(word1) and j < len(word2):
-            res.append(word1[i])
-            res.append(word2[j])
-            i+=1
-            j+=1
-        res.append(word1[i:])
-        res.append(word2[j:])
-        res = ''.join(res)
-        return res
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        merged = ""
+        i, j = 0, 0
+        m, n = len(word1), len(word2)
+        while i<m and j<n:
+            merged += word1[i]
+            merged += word2[i]
+            i += 1
+            j += 1
+
+        if i<m:
+            merged += word1[i:]
+        
+        if j<n:
+            merged += word2[j:]
+
+        return merged
